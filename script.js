73aa721bb37dbc3624f6f7e1bb3cef5e32446aa2
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-04-22 12:22:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let character = 'Hello';
 console.log(character);
-character = "World"
-console.log(character)
+character = "World";
 let secondCharacter;
+secondCharacter = "Test";
 console.log(secondCharacter);
