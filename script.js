4e202341926ba62961e96441037801509db9da9a
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-04-22 18:22:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-function padRow() {
+function padRow(rowNumber, rowCount) {
 
 }
+
 
 for (let i = 0; i < count; i = i + 1) {
   rows.push(character);
