c6488e8b53336ac20c34d60796d2f2ca8ed0e492
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-04-22 18:05:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const rows = [];
 function padRow(name) {
  return name;
 }
-const call = padRow();
+const call = padRow("Mitch");
 console.log(call);
 for (let i = 0; i < count; i = i + 1) {
   rows.push(character);
