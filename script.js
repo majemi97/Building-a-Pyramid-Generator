3604f36504a97e8788fab0f6c6c58af47dde6614
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-04-24 16:16:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
+//do a different kind of loop
 for (let i = 1; i <= count; i = i ++) {
   rows.push(padRow(i, count))
 }
